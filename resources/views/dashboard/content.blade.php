91feb509998b7bb6ecdafbc9bd14412846fccaa9
rahmat hidayat <rahmatthidayat0120@gmail.com>
2026-03-04 09:07:54 +0700

    perubahan pada git

--- resources/views/dashboard/content.blade.php
+++ resources/views/dashboard/content.blade.php
@@ -2,154 +2,80 @@
 
 @section('content')
 
+<main class="app-main">
 
-
-<div class="container">
-
-    <main class="app-main">
-        <!--begin::App Content Header-->
-        <div class="app-content-header">
-            <!--begin::Container-->
-            <div class="container-fluid">
-                <!--begin::Row-->
-                <div class="row">
-                    <div class="col-sm-6">
-                        <h3 class="mb-0" style=color:black>Dashboard Admin Perpustakaan</h3>
-                    </div>
-
+    <div class="app-content-header">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-sm-6">
+                    <h3 class="mb-0 text-dark">Dashboard Admin Perpustakaan</h3>
                 </div>
-                <!--end::Row-->
             </div>
-            <!--end::Container-->
         </div>
-        <!--end::App Content Header-->
-        <!--begin::App Content-->
+    </div>
 
-        <div class="app-wrapper">
-            <div class="app-content">
-                <!--begin::Container-->
-                <div class="container-fluid">
+    <div class="app-content">
+        <div class="container-fluid">
 
-                    <!--begin::Row-->
-                    <div class="row">
-                        <!--begin::Col-->
-                        <div class="col-lg-3 col-6">
-                            <!--begin::Small Box Widget 1-->
-                            <div class="small-box text-bg-primary">
-                                <div class="inner">
-                                    <h3>{{$totalStok}}</h3>
-                                    <p>Stok</p>
-                                </div>
-                                <svg
-                                    class="small-box-icon"
-                                    fill="currentColor"
-                                    viewBox="0 0 24 24"
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    aria-hidden="true">
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor"
-                                        viewBox="0 0 24 24" width="25" height="25">
-                                        <path d="M4 3h13a2 2 0 012 2v14a2 2 0 01-2 2H4a2 2 0 01-2-2V5a2 2 0 012-2zm0 2v14h13V5H4zm3 2h6v2H7V7zm0 4h6v2H7v-2z">
-                                    </svg>
+            {{-- ROW BOX STATISTIK --}}
+            <div class="row">
 
-                                </svg>
-
-                            </div>
-                            <!--end::Small Box Widget 1-->
+                <div class="col-lg-3 col-6">
+                    <div class="small-box text-bg-primary">
+                        <div class="inner">
+                            <h3>{{ $totalStok }}</h3>
+                            <p>Stok</p>
                         </div>
-                        <!--end::Col-->
-                        <div class="col-lg-3 col-6">
-                            <!--begin::Small Box Widget 2-->
-                            <div class="small-box text-bg-success">
-                                <div class="inner">
-                                    <h3>{{$totalDipinjam}}</h3>
-                                    <p>Dipinjam</p>
-                                </div>
-                                <svg
-                                    class="small-box-icon"
-                                    fill="currentColor"
-                                    viewBox="0 0 24 24"
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    aria-hidden="true">
-                                    <path
-                                        d="M18.375 2.25c-1.035 0-1.875.84-1.875 1.875v15.75c0 1.035.84 1.875 1.875 1.875h.75c1.035 0 1.875-.84 1.875-1.875V4.125c0-1.036-.84-1.875-1.875-1.875h-.75zM9.75 8.625c0-1.036.84-1.875 1.875-1.875h.75c1.036 0 1.875.84 1.875 1.875v11.25c0 1.035-.84 1.875-1.875 1.875h-.75a1.875 1.875 0 01-1.875-1.875V8.625zM3 13.125c0-1.036.84-1.875 1.875-1.875h.75c1.036 0 1.875.84 1.875 1.875v6.75c0 1.035-.84 1.875-1.875 1.875h-.75A1.875 1.875 0 013 19.875v-6.75z"></path>
-                                </svg>
-
-                            </div>
-                            <!--end::Small Box Widget 2-->
-                        </div>
-                        <!--end::Col-->
-                        <div class="col-lg-3 col-6">
-                            <!--begin::Small Box Widget 3-->
-                            <div class="small-box text-bg-warning">
-                                <div class="inner">
-                                    <h3>{{ $dikembalikan }}</h3>
-                                    <p>dikembalikan</p>
-                                </div>
-                                <svg
-                                    class="small-box-icon"
-                                    fill="currentColor"
-                                    viewBox="0 0 24 24"
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    aria-hidden="true">
-
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24" width="24">
-                                        <path d="M9 7l-5 5 5 5V13h6a4 4 0 010 8h-1v2h1a6 6 0 000-12H9V7z" />
-                                    </svg>
-                                </svg>
-
-                            </div>
-                            <!--end::Small Box Widget 3-->
-                        </div>
-                        <!--end::Col-->
-                        <div class="col-lg-3 col-6">
-                            <!--begin::Small Box Widget 4-->
-                            <div class="small-box text-bg-danger">
-                                <div class="inner">
-                                    <h3>{{ $belumDikembalikan }}</h3>
-                                    <p>Belum Dikembalikan</p>
-                                </div>
-                                <svg
-                                    class="small-box-icon"
-                                    fill="currentColor"
-                                    viewBox="0 0 24 24"
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    aria-hidden="true">
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24" width="25">
-                                        <path d="M5 3h10a2 2 0 012 2v4h-2V5H5v14h10v-4h2v4a2 2 0 01-2 2H5a2 2 0 01-2-2V5a2 2 0 012-2zm9.59 6L17 11.41V8h2v6h-6v-2h3.41z" />
-                                    </svg>
-
-                                </svg>
-
-                            </div>
-                            <!--end::Small Box Widget 4-->
-                        </div>
-                        <!--end::Col-->
                     </div>
-                    <!--end::Row-->
-                    <!--begin::Row-->
-                    <div class="row">
-                        <!-- Start col -->
-                        <div class="col-lg-12 connectedSortable">
-                            <div class="card mb-4">
-
-                                <div class="card-body">
-                                    <div id="revenue-chart"></div>
-                                </div>
-                            </div>
-                            <!-- /.card -->
-
-                        </div>
-
-                    </div>
-                    <!-- /.row (main row) -->
                 </div>
-                <!--end::Container-->
-            </div>
-            <!--end::App Content-->
-    </main>
-</div>
 
-<!-- script js chart -->
+                <div class="col-lg-3 col-6">
+                    <div class="small-box text-bg-success">
+                        <div class="inner">
+                            <h3>{{ $totalDipinjam }}</h3>
+                            <p>Dipinjam</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-3 col-6">
+                    <div class="small-box text-bg-warning">
+                        <div class="inner">
+                            <h3>{{ $dikembalikan }}</h3>
+                            <p>Dikembalikan</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-3 col-6">
+                    <div class="small-box text-bg-danger">
+                        <div class="inner">
+                            <h3>{{ $belumDikembalikan }}</h3>
+                            <p>Belum Dikembalikan</p>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+            {{-- ROW CHART --}}
+            <div class="row mt-4">
+                <div class="col-lg-12">
+                    <div class="card">
+                        <div class="card-body">
+                            <div id="revenue-chart"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+</main>
+
+
+{{-- SCRIPT CHART --}}
 <script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
 
 <script>
